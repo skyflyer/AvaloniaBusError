8830b453b6644636e1a6936ad090f7485761175d
Miha Valencic <miha.valencic@gmail.com>
2023-09-27 15:44:50 +0200

    Broken with "bus error"

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.Diagnostics;
 
 namespace AvaloniaBusError;
 
@@ -9,13 +10,27 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+        Trace.AutoFlush = true;
+
+        Trace.WriteLine("Program start");
+
+        try {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        } catch (Exception x) {
+            Trace.WriteLine("Exception: " + x.ToString());
+        } finally {
+            Trace.WriteLine("Program exit");
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace(Avalonia.Logging.LogEventLevel.Verbose);
 }
